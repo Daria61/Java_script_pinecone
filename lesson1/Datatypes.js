424c30adab86fffb74e319fc02c092e8e614c8bb
Daria61 <ch.erdenedari0601@gmail.com>
2022-11-28 12:33:12 +0800

    lssn1

---
+++ lesson1/Datatypes.js
@@ -0,0 +1,18 @@
+var too= 23;
+console.log(typeof too)
+var useg= "string bna"
+console.log(typeof useg )
+var unen= false
+console.log( typeof unen)
+var hooson 
+console.log( typeof hooson)
+var bhgui = null
+console.log(typeof bhgui ) 
+var too2 = 23, useg2 = "string bna", unen2 = false, hooson2 , bhgui2 = null 
+console.log( typeof too2 )
+console.log( typeof useg2 )
+console.log( typeof unen2 )
+
+var MyAge = 16;
+var yourAge = 23;
+
