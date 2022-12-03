2e1116229c707dc7549e9eb201d787cb68a282ec
Daria61 <ch.erdenedari0601@gmail.com>
2022-12-03 12:21:39 +0800

    fromhome

--- loop/whileDo/PrimeNumber.js
+++ loop/whileDo/PrimeNumber.js
@@ -1,23 +1,34 @@
-let n = prompt("too oruul ");k =0, res = false
-while(k <= 100){
-    if (n != k){
-        if (n % k == 0 ){
-            res = true 
-         } else {
-             res = false 
-         }
-    } else {
-        
-    k++
+let n = prompt("too oruul ");k =2, res = true
+while(n > k && n < k && k < 100){
+    if (n % k == 0 ){
+        res= false
+    }
+    k++;
 }
 
+// while(k <= 100){
+//     if (k < n){
+//         if (n % k == 0 ){
+//             res = true 
+//          } else {
+//              res = false 
+//          }
+//     } else if (k > n){
+//         if (n % k == 0 ){
+//             res = true 
+//          } else {
+//              res = false 
+//          }
+//     k++
+//     }
+// }
 
 if (res){
-    console.log("Prime bish ");
+    console.log("mun ");
 } else {
-    console.log("mun");
+    console.log("bish");
 }
-}
+
 
 // if (k % 2 !== 0 &&  k % 3 !== 0 && k % 5 !== 0 && k % 7 !== 0){
 //     if (n == k){
---
+++ loop/whileDo/loop.js
@@ -0,0 +1,32 @@
+//1
+let a = 0
+while (a < 10){
+    console.log(a);
+    a++
+}
+//2 
+let b =0 
+while (a < 10){
+    if (a % 2 !== 0){
+        console.log(a);
+    }
+    a ++;
+}
+//3 
+let c =0 
+while (a < 10){
+    if (a % 2 == 0){
+        console.log(a);
+    }
+    a ++;
+}
+
+//4 
+let d = 0, e = 0;
+while (a < 100){
+    e = e + a;
+    a ++
+}
+console.log(e);
+
+//5
