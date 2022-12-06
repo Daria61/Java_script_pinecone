73899594aac2ebf4fa11ccc71375e581f497c081
Daria61 <ch.erdenedari0601@gmail.com>
2022-12-06 09:12:31 +0800

    lsnn

---
+++ loop/whileDo/fibonacci.js
@@ -0,0 +1,14 @@
+let heddeh = 2, gar = 30, a =0 , b = 1, c =0, gishuun = 2;
+if (gar > 0){
+    console.log(  "1  gishuun " + b);
+    while (gar >= heddeh){
+        c = a + b ;
+        console.log( gishuun + " gishuun " + c);
+        a =  b;
+        b = c ;
+        heddeh ++ ; 
+        gishuun ++;
+    }
+}
+
+
