9092ed747ca2e8bae97130cec7699cfa95b3837d
Daria61 <ch.erdenedari0601@gmail.com>
2022-12-06 12:48:34 +0800

    inschool

--- ArrayDate/DavhardsanUtga.js
+++ ArrayDate/DavhardsanUtga.js
@@ -1,9 +1,9 @@
-// let simple = [4, 2, 34, 4, 1, 12, 1, 4], w = 0;
-// for(i = 0; i < simple.length; i ++){
-//     for(j = 0 ; j , simple.length; j ++){
-//         if (simple[i] == simple[i]){
-//             w ++
-//         }
-//     }
-// }
-// console.log(w);
+let simple = [4, 2, 34, 4, 1, 12, 1, 4];
+for(i = 0; i < simple.length; i ++){
+    for(j = 0 ; j< simple.length; j ++){
+        if (simple[i] == simple[j]){
+            console.log(simple[i]);
+            break
+        } 
+    }
+}
