13972d35d9d6160dd12b791edb77259cf1a19783
Daria61 <ch.erdenedari0601@gmail.com>
2022-12-06 18:53:42 +0800

    hhhh

---
+++ function/simple.js
@@ -0,0 +1,39 @@
+// function FullName(name){
+//     console.log(" Hi " + name);
+// }
+// FullName("Daria")
+
+// let FullName = function (name ){
+//     console.log(" Hi " + name );
+// }
+// FullName("Daria")
+
+// let FullName = (flname) => {
+//     console.log(" Hi " + flname);
+// }
+// FullName("Daria ")
+
+// let FullName = (ner) => console.log(" hi " + ner);
+// FullName("Daria")
+
+// function fullname(firstName , lastName){
+//     console.log(" hi Nice to meet you " + lastName + " " +  firstName);
+// }
+// fullname("Erdenedari", "Chuluunbaatar")
+
+// let fulname = (firstName, lastName ) => console.log("Hi nice to meet you " + lastName + " " + firstName); 
+// fulname("Erdenedari", "Chuluunbaatar")
+// function sum (a , b ){
+//     return a + b 
+// }
+// console.log(sum(10,22));
+
+// function rectangleArea(a , b){
+//     console.log("talbai ni "  + (S = a * b));
+// }
+// rectangleArea(10 , 6) 
+
+// function rectanglePer(a , b){
+//     console.log("Perimeter ni " + (2 * ( a + b )));
+// }
+// rectanglePer(22 , 2 )
