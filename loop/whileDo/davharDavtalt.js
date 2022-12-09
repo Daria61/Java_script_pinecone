4f0a8321b89d0aba43101864e2bec9a7239836db
Daria61 <ch.erdenedari0601@gmail.com>
2022-12-09 14:36:15 +0800

    com

--- loop/whileDo/davharDavtalt.js
+++ loop/whileDo/davharDavtalt.js
@@ -1,16 +1,16 @@
-// let urtni = 5, string = " ";
-// for (let i = 1; i <= urtni; i++){
-//     string = string +" " + i 
-//     console.log(string);
-// }
-
-let start = 5 , string = " ", e = 0 , davtalt = start;
-while (davtalt > 0){
-    e ++
-    for (let i = start; i >= e; i--){
-        string = string + " " + i 
-    }
+let urtni = 5, string = " ";
+for (let i = 1; i <= urtni; i++){
+    string = string +" " + i 
     console.log(string);
-    string = " "
-    davtalt --
 }
+
+// let start = 5 , string = " ", e = 0 , davtalt = start;
+// while (davtalt > 0){
+//     e ++
+//     for (let i = start; i >= e; i--){
+//         string = string + " " + i 
+//     }
+//     console.log(string);
+//     string = " "
+//     davtalt --
+// }
