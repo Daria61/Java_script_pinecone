3e54a408b94cf38ec69d943d9c69fe642469d5f2
Daria61 <ch.erdenedari0601@gmail.com>
2022-12-12 12:46:39 +0800

    nm

--- ArrayDate/ArrayMethod/palindromic.js
+++ ArrayDate/ArrayMethod/palindromic.js
@@ -1,10 +1,22 @@
-let input = "123321"
+let input = "1254321"
 if (input.length % 2 == 0){
     sub = input.substring(0, input.length / 2)
     split = sub.split("")
     equal =  split.reverse()
-    equal.join("")
-    console.log(equal);
+    joined = equal.join("")
+    if(joined == input.substring(input.length / 2 )){
+        console.log("true");
+    } else {
+        console.log("false");
+    }
 }else {
-
+    sub = input.substring(0, Math.floor(input.length / 2))
+    split = sub.split("")
+    equal =  split.reverse()
+    joined = equal.join("")
+    if(joined == input.substring(Math.floor(input.length / 2 + 1) )){
+        console.log("true");
+    } else {
+        console.log("false");
+    }
 }
