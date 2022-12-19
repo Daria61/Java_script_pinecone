4b52c333e50661eb4147b1fcafbcfcace8abb44d
Daria61 <ch.erdenedari0601@gmail.com>
2022-12-19 14:43:00 +0800

    ToDo

--- ToDoListProject/List.js
+++ ToDoListProject/List.js
@@ -8,18 +8,18 @@ head.appendChild(headtit)
     let d = new Date();
     let year = d.getFullYear()
     let month = d.getMonth()
-    let day = d.getDay()
+    let day = d.getDate()
     let hour =d.getHours()
     let min = d.getMinutes()
     let date = document.createElement("p")
     head.appendChild(date)
-    date.innerHTML = `${year}оны ${month}сарын ${day}өдөр  ${hour}цаг ${min}мин ` 
+    date.innerHTML = `${year}оны ${month}сарын${day}өдөр  ${hour}цаг ${min}мин ` 
     tsag()
 function tsag(){
     let d = new Date();
     let year = d.getFullYear()
     let month = d.getMonth()
-    let day = d.getDay()
+    let day = d.getDate()
     let hour =d.getHours()
     let min = d.getMinutes()
     date.innerHTML = `${year}оны ${month + 1}сарын ${day}өдөр  ${hour}цаг ${min}мин ` 
@@ -28,7 +28,7 @@ function tsag(){
 // list too 
 let listToo = 0
 let too = document.createElement("p")
-too.innerHTML = `Нийт ${listToo} жагсаалт`
+too.innerHTML = `Нийт ${listToo}жагсаалт`
 head.appendChild(too)
 // add 
 let input = document.createElement("input")
@@ -38,7 +38,6 @@ let btn = document.createElement("button")
 btn.innerHTML = "Нэмэх"
 head.appendChild(btn)
 btn.addEventListener("click", nemeh)
-
 //  list show 
 let secTwo = document.getElementById("list") 
 let jagsaaltTit = document.createElement("h1")
@@ -46,47 +45,56 @@ jagsaaltTit.innerHTML ="Жагсаалт"
 secTwo.appendChild(jagsaaltTit)
 let listContain = document.createElement("div")
 list.appendChild(listContain)
-
-
 function nemeh (){
     let utga = document.getElementsByTagName("input")[0].value
    if (utga.length > 1){
     listToo++
-    
     let text = document.createElement("div")
     let checkBox = document.createElement("input")
     checkBox.setAttribute("type", "checkbox")
+    console.log(checkBox.value);
     let pp = document.createElement("input")
     pp.placeholder = utga
     pp.readOnly = true
     pp.className = "listItem"
-
     let icontg = document.createElement("div")
     icontg.innerHTML = '<i class="bi bi-trash3"></i>'
-
     let iconSearch = document.createElement("div")
     iconSearch.innerHTML = '<i class="bi bi-pencil"></i>'
-
     listContain.appendChild(text)
     text.appendChild(checkBox)
     text.appendChild(pp)
     text.appendChild(icontg)
     text.appendChild(iconSearch)
-
-    
     document.getElementsByTagName("input")[0].value = ""
-    too.innerHTML = `Нийт ${listToo} жагсаалт`
+    too.innerHTML = `Нийт${listToo}жагсаалт`
     icontg.addEventListener("click", function(){
+        listContain.removeChild(text)
+        listToo-- 
+        too.innerHTML = `Нийт${listToo}жагсаалт`
 
     })
     iconSearch.addEventListener("click", function(){
-        pp.setAttribute = ("read-only", false)
+        text.style.backgroundColor = " #4F5B9BFF"
+        pp.style.backgroundColor = "  #4F5B9BFF"
+        pp.readOnly = false
+        fixedTit = pp.placeholder
+        pp.value = fixedTit
+        pp.addEventListener("keypress", function(e){
+            if (e.keyCode === 13){
+                pp.readOnly = true
+                text.style.backgroundColor = "#5F4B8BFF "
+                pp.style.backgroundColor = "#5F4B8BFF "
+            }
+        })
+    })
+    checkBox.addEventListener("click", function(){
+        if (checkBox.checked == true ){
+            listContain.removeChild(text)
+        } 
     })
    
 }
 }
 
-// function deleted(){
-    
-// }
 
